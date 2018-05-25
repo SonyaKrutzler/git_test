aa8371e50aa4b949b5760163ca6c87ab8f1c6c78
SonyaKrutzler <sonyakrutzler@gmail.com>
2018-05-25 11:26:40 -0400

    class practice

---
+++ script.js
@@ -0,0 +1,12 @@
+
+
+
+//nested for-loop??
+
+for (let i = 0; i < 16 * 16; i++) {
+    //create div
+    const container = document.querySelector('.grid');
+    const content = document.createElement('div');
+
+    //put inside grid (add class name for div)
+}
