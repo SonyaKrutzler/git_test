918cf90f647fbb8d03b3a9fa2c72df62ca5981bd
SonyaKrutzler <sonyakrutzler@gmail.com>
2018-05-29 01:52:14 -0400

    working grid

--- script.js
+++ script.js
@@ -1,12 +1,45 @@
 
 
+const grid = document.querySelector('.grid');
 
-//nested for-loop??
+let gridSize = 16;
+grid.style.gridTemplate = "repeat(" + gridSize + ", 1fr) / repeat(" + gridSize + ", 1fr)";
 
-for (let i = 0; i < 16 * 16; i++) {
-    //create div
-    const container = document.querySelector('.grid');
-    const content = document.createElement('div');
 
-    //put inside grid (add class name for div)
+
+for (let i = 0; i < gridSize * gridSize; i++) {
+    const mysquare = document.createElement('div');
+    mysquare.classList.add('box');
+    mysquare.addEventListener("mouseover", mouseOver);
+    grid.appendChild(mysquare);
 }
+
+
+function mouseOver(e) {
+	e.srcElement.style.background = getRandomColor();
+}
+
+
+function resetGrid() {
+	document.querySelectorAll('.box');
+    box.style.backgroundColor = "";
+
+}
+
+function resizeGrid () {
+	grid.style.gridTemplate = "repeat(" + gridSize + ", 1fr) / repeat(" + gridSize + ", 1fr)";
+}
+
+ 
+function getRandomColor() {
+  var letters = '0123456789ABCDEF';
+  var color = '#';
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
+
+
+
